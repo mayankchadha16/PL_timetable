61f7720ca63c22ffa59ef8a4977cc5d13e4a7df3
mayankchadha16 <chadhamayank1609@gmail.com>
2023-03-12 21:39:30 +0530

    add other tweaks

--- timetable.ml
+++ timetable.ml
@@ -30,16 +30,22 @@ let student_to_course = [(1, [1; 2]); (2, [2; 3]); (3, [1])]
 
 (* INFERENCE GRAPH *)
 (* ############################################################################################################## *)
+  let has_common_students students1 students2 =
+    List.exists (fun x -> List.mem x students2) students1
+
+  let is_edge_valid course1 students1 course2 students2 =
+    course1 < course2 && has_common_students students1 students2
+  
   let create_inference_graph course_to_student_list =
     let edges = List.fold_left (fun acc (course1, students1) ->
       List.fold_left (fun acc' (course2, students2) ->
-        if course1 < course2 && List.exists (fun x -> List.mem x students2) students1 then
+        if is_edge_valid course1 students1 course2 students2 then
           (course1, course2) :: acc'
         else
           acc'
           ) acc course_to_student_list
           ) [] course_to_student_list in
-          List.rev edges
+          edges
 
   let edge_list = create_inference_graph course_to_student_list
 (* ############################################################################################################## *)
--- timetable.mli
+++ timetable.mli
@@ -6,6 +6,8 @@ val group_by_course : ('a * 'b list) list -> 'a * 'b -> ('a * 'b list) list
 val course_to_student : ('a * 'b list) list -> ('b * 'a list) list
 
 (* INFERENCE GRAPH *)
+val has_common_students : 'a list -> 'a list -> bool
+val is_edge_valid : 'a -> 'b list -> 'a -> 'b list -> bool
 val create_inference_graph : ('a * 'b list) list -> ('a * 'a) list
 
 (* PRINT TIMETABLE (Converter) *)
